6aa53fc3d9002d3f87a2a1c04214eddf1284c34c
@zengmengxuan <zengmengxuan@hello.com>
2020-12-01 16:17:53 +0800

    两种导航模式

--- src/components/nav/nav.vue
+++ src/components/nav/nav.vue
@@ -2,13 +2,19 @@
  * @Author: zmx
  * @Date: 2020-11-30 10:45:01
  * @LastEditors: zmx
- * @LastEditTime: 2020-12-01 16:05:17
+ * @LastEditTime: 2020-12-01 16:17:02
  * @FilePath: /vue-pro/src/components/nav/nav.vue
 -->
 <template>
   <div id="nav-page">
-    <li @click="goToPage" data-type="home">首页</li>
-    <li @click="goToPage" data-type="login">登录</li>
+    <!-- <li @click="goToPage" data-type="home">首页</li>
+    <li @click="goToPage" data-type="login">登录</li> -->
+    <li>
+      <router-link to="/home">首页</router-link>
+    </li>
+    <li>
+      <router-link to="/login">登录</router-link>
+    </li>
     <div>
       Clicked: {{ $store.state.navModule.count }} times, count is {{ evenOrOdd }}.
       <button @click="increment">+</button>
